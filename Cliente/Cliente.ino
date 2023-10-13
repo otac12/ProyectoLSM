6a897261aec0c9a0d7598717d2c62fb47ffc5223
elisa Guerra Santibáñes <eliguesan2904@gmail.com>
2023-10-12 18:50:58 -0600

    ¨Cliente¨

---
+++ Cliente/Cliente.ino
@@ -0,0 +1,35 @@
+//ESP32S3 que funge como cliente
+
+#include <WiFi.h>
+
+// Se declaran las credenciales del servidor
+
+const char* SSID = "Maestro";
+const char* PASS = "Master123";
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println();
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  initWiFi();
+}
+
+void loop() {
+  
+}
