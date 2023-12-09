493e83d32b4028ab58bd384d9c82a363646ff218
otac12 <otac@hotmail.com>
2023-12-09 12:52:05 -0600

    Restructuracion

--- Cliente/Cliente.ino
+++ Cliente/Cliente.ino
@@ -1,51 +1,33 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+
 #include "esp_camera.h"
-
-<<<<<<< HEAD
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
-
-const char* ssid = "MiRedAP";
-const char* password = "MiContraseñaAP";
-const char* serverAddress = "0.0.0.0"; // Reemplaza con la dirección IP del servidor en modo AP
-=======
-const char* ssid = "Esp32";
-const char* password = "Tonatiuh";
-const char* serverAddress = "192.168.4.1"; // Reemplaza con la dirección IP del servidor en modo AP
->>>>>>> 541f29aa2b15b3f572cb5c3e56a7d4762675b773
-
-#define CAMERA_MODEL_XIAO_ESP32S3
+#include <WiFi.h>
+#include <FS.h>
+#include <SPIFFS.h> 
+#define CAMERA_MODEL_AI_THINKER
 #include "Pines.h"
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
+const char* ssid = "Wi-Fi IPN";
+const char* password = "";
+static const char *TAG = "camera_example";
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando a la red Wi-Fi...");
+void setup() {
+
+  Serial.begin(115200);
+
+  if(!SPIFFS.begin(true)){
+    Serial.println("Error al montar SPIFFS");
+    return;
   }
 
-  Serial.println("Conexión Wi-Fi establecida");
+  // Conéctate a la red WiFi
+  //WiFi.begin(ssid, password);
+  //while (WiFi.status() != WL_CONNECTED) {
+   // delay(1000);
+   // Serial.println("Conectando a WiFi...");
+  //}
+  //Serial.println("Conectado a la red WiFi");
 
-  // camara
-
+  // Configura la cámara
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -66,76 +48,59 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG; // for streaming
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.frame_size = FRAMESIZE_VGA;
+  //config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
-  
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    if(psramFound()){
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
+  // Inicializa la cámara
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Error al inicializar la cámara: %s\n", esp_err_to_name(err));
+    Serial.printf("Error al inicializar la cámara (%d)", err);
     return;
   }
+
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-        // Captura una imagen con la cámara
-        camera_fb_t *fb = esp_camera_fb_get();
-        if (fb) {
-            WiFiClient Cliente;
-            HTTPClient http;
-            http.begin(Cliente,serverAddress,80, "/upload");
-            // Configura la cabecera de la solicitud HTTP para el archivo JPEG
-            http.addHeader("ContentType","image/jpeg");
 
-           Serial.print(http.header("ContentType"));
-            // Envía la imagen al servidor
-            int httpResponseCode = http.POST(fb->buf, fb->len);
+  camera_fb_t * fb = esp_camera_fb_get();
 
-            if (httpResponseCode == 200) {
-                Serial.println("Imagen enviada con éxito al servidor");
-            } else {
-                Serial.print("Error al enviar la imagen. Código de respuesta: ");
-                Serial.println(httpResponseCode);
-            }
+  if (!fb) {
+    ESP_LOGE(TAG, "Error al capturar el frame de la cámara");
+    return;
+  }
 
-            http.end();
-            esp_camera_fb_return(fb);
-        } else {
-            Serial.println("Error al capturar la imagen.");
-        }
-    }
-  
-  // Espera un tiempo antes de capturar otra imagen
- 
+  // Guarda la foto en formato BMP
+  fs::File file = SPIFFS.open("/foto.bmp", FILE_WRITE);
+  if(!file){
+    Serial.println("Error al abrir el archivo");
+    return;
+  }
+
+  file.write((const uint8_t*)fb->buf, fb->len);
+  file.close();
+
+  file = SPIFFS.open("/foto.bmp", FILE_READ);
+  if(!file){
+    Serial.println("Error al abrir el archivo para lectura");
+    return;
+  }
+  Serial.println("Contenido del archivo 'foto.bmp':");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+
+  // Libera la memoria utilizada por el búfer de la cámara
+  esp_camera_fb_return(fb);
+
+  Serial.println("Foto capturada y guardada como 'foto.bmp'");
+
+  // Espera 5 segundos antes de tomar otra foto
   delay(10000);
+  
 }
