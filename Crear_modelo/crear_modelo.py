12e8fec4fbea870b2300c781db9e0534e1f9391e
otac12 <otac@hotmail.com>
2024-06-24 17:25:38 -0600

    actualizacion de script

--- Crear_modelo/crear_modelo.py
+++ Crear_modelo/crear_modelo.py
@@ -6,6 +6,12 @@ IMAGES_PATH = "../Imagenes"
 
 if os.path.exists(IMAGES_PATH):
     print("El archivo fue encontrado en la ruta especificada.")
+    print(IMAGES_PATH)
+    labels = []
+    for i in os.listdir(IMAGES_PATH):
+        if os.path.isdir(os.path.join(IMAGES_PATH, i)):
+            labels.append(i)
+    print(labels)
 else:
     print("El archivo no fue encontrado en la ruta especificada.")
 
@@ -17,7 +23,7 @@ data = gesture_recognizer.Dataset.from_folder(
 train_data, rest_data = data.split(0.8)
 validation_data, test_data = rest_data.split(0.5)
 
-hparams = gesture_recognizer.HParams(export_dir="LSM_model")
+hparams = gesture_recognizer.HParams(export_dir="lsm_model")
 options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
 model = gesture_recognizer.GestureRecognizer.create(
     train_data=train_data,
@@ -28,11 +34,4 @@ model = gesture_recognizer.GestureRecognizer.create(
 loss, acc = model.evaluate(test_data, batch_size=1)
 print(f"Test loss:{loss}, Test accuracy:{acc}")
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model._model)
-tflite_model = converter.convert()
-
-# Guardar el modelo TensorFlow Lite en un archivo
-with open('LSM.tflite', 'wb') as f:
-    f.write(tflite_model)
-
-print("Modelo TensorFlow Lite exportado exitosamente como modelo_entrenado.tflite")
+model.export_model()
