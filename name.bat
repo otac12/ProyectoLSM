83365e6cc7697c03445cbe608e6c492b2fbea31c
Guerra Santibanez Elisa (BGSW/EBS3.1-SX) <Elisa.GuerraSantibanez@mx.bosch.com>
2024-07-28 23:31:39 -0600

    Horror

--- name.bat
+++
@@ -1,9 +0,0 @@
-setlocal enabledelayedexpansion
-
-for /d %%d in (*) do (
-    set contador=1
-    for %%f in ("%%d\*.jpg") do (
-        ren "%%f" "!contador!.jpg"
-        set /a contador+=1
-    )
-)
